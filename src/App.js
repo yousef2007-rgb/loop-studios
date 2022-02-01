9d616b4c8d38ccf09d38198804e8243be458d654
Yousef Aburadi <yousefaburadi@yahoo.com>
2022-02-01 11:38:09 +0200

    last-commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ function App() {
         <Interactive/>
         <Creations/>
         <Footer/>
+        <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://github.com/yousef2007-rgb">Yousef Aburadi</a>.
+  </div>
     </div>
   );
 }
